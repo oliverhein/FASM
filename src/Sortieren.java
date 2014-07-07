390ec017f98565f9c11d3f4dbe0ebf7c6b6474d8
ohein <oliver.hein@t-online.de>
2014-07-07 22:11:57 +0200

    dfgdfg

--- src/Sortieren.java
+++ src/Sortieren.java
@@ -4,7 +4,7 @@ public class Sortieren {
 	public static void main(String[] args) {
 		
 		int[] feld = new int[100000]; // 100.000 ganze Zahlen werden sortiert
-		long start, finish; // Variablen fuer Zeitmessung
+		long start, finish; // Variablen für Zeitmessung
 		
 		for( int i=0; i<100000; i++ ) // Zufallszahlen generieren
 			feld[i] = (int)(Math.random() * 32000);	
