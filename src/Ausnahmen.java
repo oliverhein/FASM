bed160a9cd0247a4dfb91f9d0d0f75d00b172ba4
ohein <oliver.hein@t-online.de>
2014-07-07 13:10:10 +0200

    ghghg

--- src/Ausnahmen.java
+++ src/Ausnahmen.java
@@ -2,7 +2,19 @@
 public class Ausnahmen {
 
 	public static void main(String[] args) {
+		int i=0;
+		int [] feld = new int[5];
 		
+		try {		
+			for (i=0;i<=5;i++)
+				feld[i]=i;
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Indexfehler! Laufindex=" + i + " nicht im Bereich!");
+		}
+		finally {
+			// nichts zu tun
+		}
 	}
 
 }
